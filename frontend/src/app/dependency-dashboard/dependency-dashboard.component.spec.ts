a0307f03c36112ba1eda44f0f4faddddfe729fec
anielrossi <rssnla93@gmail.com>
2026-08-22 00:35:06 +0200

    declared functions

--- frontend/src/app/dependency-dashboard/dependency-dashboard.component.spec.ts
+++ frontend/src/app/dependency-dashboard/dependency-dashboard.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DependencyDashboardComponent } from './dependency-dashboard.component';
 
+declare function describe(description: string, specDefinitions: () => void): void;
+declare function beforeEach(action: () => void | Promise<void>): void;
+declare function it(description: string, testFunction: () => void | Promise<void>): void;
+declare function expect<T>(actual: T): { toBeTruthy(): void };
+
 describe('DependencyDashboardComponent', () => {
   let component: DependencyDashboardComponent;
   let fixture: ComponentFixture<DependencyDashboardComponent>;
